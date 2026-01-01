0bf44d51e98830b07c0aea4bd625dabfed905ff4
adeebkhans <mdadeebkhan01@gmail.com>
2026-01-01 17:30:16 +0530

    update: Task model and migration for task attributes

--- database/migrations/2026_01_01_080317_create_tasks_table.php
+++ database/migrations/2026_01_01_080317_create_tasks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title'); // Required text
+            $table->text('description')->nullable(); // Optional text
+            $table->enum('priority', ['Low', 'Medium', 'High'])->default('Medium'); // Dropdown choice
+            $table->boolean('is_completed')->default(false); // Status (Pending/Completed)
+            $table->timestamps(); // Created at & Updated at
         });
     }
 
